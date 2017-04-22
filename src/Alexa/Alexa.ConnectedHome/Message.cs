19cce208d266b164b4049dd7e123dc9bba05a483
Elton FAN <chuangen@live.com>
2017-04-22 22:38:56 +0800

    添加 Alexa Smart Home Skill 消息类项目。

---
+++ src/Alexa/Alexa.ConnectedHome/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alexa.ConnectedHome
+{
+    /// <summary>
+    /// The skill adapter directive.
+    /// </summary>
+    public class Message
+    {
+        public MessageHeader Header { get; set; }
+        public MessagePayload Payload { get; set; }
+    }
+}
---
+++ src/Alexa/Alexa.ConnectedHome/MessagePayload.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alexa.ConnectedHome
+{
+    /// <summary>
+    /// The payload for a skill adapter directive.
+    /// </summary>
+    public class MessagePayload
+    {
+    }
+}
