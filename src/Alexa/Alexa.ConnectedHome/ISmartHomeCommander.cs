b8170f4f51ecde7523bda6c591f707d4f1e43768
Elton FAN <chuangen@live.com>
2017-04-24 19:12:51 +0800

    实现 Alexa Smart Home Skill 指令客户端（功能的请求方）基类，以简化客户端代码。

---
+++ src/Alexa/Alexa.ConnectedHome/ISmartHomeCommander.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alexa.ConnectedHome
+{
+    public interface ISmartHomeCommander
+    {
+        //On/Off Messages
+        void TurnOn(string accessToken, Appliance appliance);
+        void TurnOff(string accessToken, Appliance appliance);
+        //Tunable Lighting Control Messages
+        LightColor SetColor(string accessToken, Appliance appliance, LightColor color);
+        int SetColorTemperature(string accessToken, Appliance appliance, int colorTemperature);
+        int IncrementColorTemperature(string accessToken, Appliance appliance);
+        int DecrementColorTemperature(string accessToken, Appliance appliance);
+        //Door Lock Control and Query Messages 
+        ApplianceLockState GetLockState(string accessToken, Appliance appliance, out DateTime applianceResponseTimestamp);
+        ApplianceLockState SetLockState(string accessToken, Appliance appliance, ApplianceLockState lockState);
+        //Temperature Control and Query Messages
+        float GetTemperatureReading(string accessToken, Appliance appliance, out DateTime applianceResponseTimestamp);
+        void GetTargetTemperature(string accessToken, Appliance appliance,
+            out float targetTemperature, out TemperatureModeData temperatureMode, out DateTime applianceResponseTimestamp);
+        void SetTargetTemperature(string accessToken, Appliance appliance, float targetTemperature,
+            out TemperatureState currentState, out TemperatureState previousState);
+        void IncrementTargetTemperature(string accessToken, Appliance appliance, float deltaTemperature,
+            out TemperatureState currentState, out TemperatureState previousState);
+        void DecrementTargetTemperature(string accessToken, Appliance appliance, float deltaTemperature,
+            out TemperatureState currentState, out TemperatureState previousState);
+        //Percentage Messages
+        void SetPercentage(string accessToken, Appliance appliance, float percentage);
+        void IncrementPercentage(string accessToken, Appliance appliance, float deltaPercentage);
+        void DecrementPercentage(string accessToken, Appliance appliance, float deltaPercentage);
+        //Health Check Messages
+        bool HealthCheck(long initiationTimestamp, out string description);
+    }
+}
---
+++ src/Alexa/Alexa.ConnectedHome/SmartHomeCommander.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alexa.ConnectedHome
+{
+    public abstract class AbstractSmartHomeCommander : ISmartHomeCommander
+    {
+        protected virtual void Log(LogLevel level, string format, params object[] arguments)
+        {
+            Console.WriteLine(level.ToString().ToUpper() + ": " + string.Format(format, arguments));
+        }
+        protected virtual void LogDebug(string format, params object[] arguments)
+        {
+            Log(LogLevel.Debug, format, arguments);
+        }
+        protected virtual void LogInfo(string format, params object[] arguments)
+        {
+            Log(LogLevel.Info, format, arguments);
+        }
+        protected virtual void LogWarn(string format, params object[] arguments)
+        {
+            Log(LogLevel.Warn, format, arguments);
+        }
+        protected virtual void LogError(string format, params object[] arguments)
+        {
+            Log(LogLevel.Error, format, arguments);
+        }
+
+        protected abstract MessagePayload ProgressMessage(MessagePayload request);
+
+
+        public ApplianceDetails[] DiscoverAppliances(string accessToken)
+        {
+            LogInfo("Action: DiscoverAppliances");
+
+            var request = new Discovery.DiscoverAppliancesRequest
+            {
+                AccessToken = accessToken,
+            };
+
+            var response = ProgressMessage(request) as Discovery.DiscoverAppliancesResponse;
+            return response.DiscoveredAppliances;
+        }
+
+        //On/Off Messages
+        public void TurnOn(string accessToken, Appliance appliance)
+        {
+            LogInfo("Action: TurnOn");
+
+            var request = new Control.TurnOnRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+            };
+
+            var response = ProgressMessage(request) as Control.TurnOnConfirmation;
+        }
+
+        public void TurnOff(string accessToken, Appliance appliance)
+        {
+            LogInfo("Action: TurnOff");
+
+            var request = new Control.TurnOffRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+            };
+
+            var response = ProgressMessage(request) as Control.TurnOffConfirmation;
+        }
+
+        //Tunable Lighting Control Messages
+
+        public LightColor SetColor(string accessToken, Appliance appliance, LightColor color)
+        {
+            LogInfo("Action: SetColor");
+
+            var request = new Control.SetColorRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+                 Color = color,
+            };
+
+            var response = ProgressMessage(request) as Control.SetColorConfirmation;
+            return response.AchievedState.Color;
+        }
+
+        public int SetColorTemperature(string accessToken, Appliance appliance, int colorTemperature)
+        {
+            LogInfo("Action: SetColorTemperature");
+
+            var request = new Control.SetColorTemperatureRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+                 ColorTemperature = new ControlParameter<int>(colorTemperature),
+            };
+
+            var response = ProgressMessage(request) as Control.SetColorTemperatureConfirmation;
+            return response.AchievedState.ColorTemperature.Value;
+        }
+
+
+        public int IncrementColorTemperature(string accessToken, Appliance appliance)
+        {
+            LogInfo("Action: IncrementColorTemperature");
+
+            var request = new Control.IncrementColorTemperatureRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+            };
+
+            var response = ProgressMessage(request) as Control.IncrementColorTemperatureConfirmation;
+            return response.AchievedState.ColorTemperature.Value;
+        }
+
+        public int DecrementColorTemperature(string accessToken, Appliance appliance)
+        {
+            LogInfo("Action: DecrementColorTemperature");
+
+            var request = new Control.DecrementColorTemperatureRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+            };
+
+            var response = ProgressMessage(request) as Control.DecrementColorTemperatureConfirmation;
+            return response.AchievedState.ColorTemperature.Value;
+        }
+
+        //Door Lock Control and Query Messages 
+
+        public ApplianceLockState GetLockState(string accessToken, Appliance appliance, out DateTime applianceResponseTimestamp)
+        {
+            LogInfo("Action: GetLockState");
+
+            var request = new Query.GetLockStateRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+            };
+
+            var response = ProgressMessage(request) as Query.GetLockStateResponse;
+            applianceResponseTimestamp = response.ApplianceResponseTimestamp;
+            return response.LockState;
+        }
+
+        public ApplianceLockState SetLockState(string accessToken, Appliance appliance, ApplianceLockState lockState)
+        {
+            LogInfo("Action: SetLockState");
+
+            var request = new Control.SetLockStateRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+                 LockState = lockState,
+            };
+
+            var response = ProgressMessage(request) as Control.SetLockStateConfirmation;
+            return response.LockState;
+        }
+
+        //Temperature Control and Query Messages
+        public float GetTemperatureReading(string accessToken, Appliance appliance, out DateTime applianceResponseTimestamp)
+        {
+            LogInfo("Action: GetTemperatureReading");
+
+            var request = new Query.GetTemperatureReadingRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+            };
+
+            var response = ProgressMessage(request) as Query.GetTemperatureReadingResponse;
+            applianceResponseTimestamp = response.ApplianceResponseTimestamp;
+            return response.TemperatureReading.Value;
+        }
+
+        public void GetTargetTemperature(string accessToken, Appliance appliance, out float targetTemperature, out TemperatureModeData temperatureMode, out DateTime applianceResponseTimestamp)
+        {
+            LogInfo("Action: GetTargetTemperature");
+
+            var request = new Query.GetTargetTemperatureRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+            };
+
+            var response = ProgressMessage(request) as Query.GetTargetTemperatureResponse;
+            targetTemperature = response.TargetTemperature.Value;
+            temperatureMode = response.TemperatureMode;
+            applianceResponseTimestamp = response.ApplianceResponseTimestamp;
+        }
+
+        public void SetTargetTemperature(string accessToken, Appliance appliance, float targetTemperature, out TemperatureState currentState, out TemperatureState previousState)
+        {
+            LogInfo("Action: SetTargetTemperature");
+
+            var request = new Control.SetTargetTemperatureRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+                TargetTemperature = new ControlParameter(targetTemperature),
+            };
+
+            var response = ProgressMessage(request) as Control.SetTargetTemperatureConfirmation;
+            currentState = new TemperatureState
+            {
+                TargetTemperature = response.TargetTemperature,
+                Mode = response.TemperatureMode,
+            };
+            previousState = response.PreviousState;
+        }
+
+        public void IncrementTargetTemperature(string accessToken, Appliance appliance, float deltaTemperature, out TemperatureState currentState, out TemperatureState previousState)
+        {
+            LogInfo("Action: IncrementTargetTemperature");
+
+            var request = new Control.IncrementTargetTemperatureRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+                DeltaTemperature = new ControlParameter(deltaTemperature),
+            };
+
+            var response = ProgressMessage(request) as Control.IncrementTargetTemperatureConfirmation;
+            currentState = new TemperatureState
+            {
+                TargetTemperature = response.TargetTemperature,
+                Mode = response.TemperatureMode,
+            };
+            previousState = response.PreviousState;
+        }
+
+        public void DecrementTargetTemperature(string accessToken, Appliance appliance, float deltaTemperature, out TemperatureState currentState, out TemperatureState previousState)
+        {
+            LogInfo("Action: DecrementTargetTemperature");
+
+            var request = new Control.DecrementTargetTemperatureRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+                DeltaTemperature = new ControlParameter(deltaTemperature),
+            };
+
+            var response = ProgressMessage(request) as Control.DecrementTargetTemperatureConfirmation;
+            currentState = new TemperatureState
+            {
+                TargetTemperature = response.TargetTemperature,
+                Mode = response.TemperatureMode,
+            };
+            previousState = response.PreviousState;
+        }
+
+        //Percentage Messages
+
+        public void SetPercentage(string accessToken, Appliance appliance, float percentage)
+        {
+            LogInfo("Action: SetPercentage");
+
+            var request = new Control.SetPercentageRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+                PercentageState = new ControlParameter(percentage),
+            };
+
+            var response = ProgressMessage(request) as Control.SetPercentageConfirmation;
+        }
+
+        public void IncrementPercentage(string accessToken, Appliance appliance, float deltaPercentage)
+        {
+            LogInfo("Action: IncrementPercentage");
+
+            var request = new Control.IncrementPercentageRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+                DeltaPercentage = new ControlParameter(deltaPercentage),
+            };
+
+            var response = ProgressMessage(request) as Control.IncrementPercentageConfirmation;
+        }
+
+        public void DecrementPercentage(string accessToken, Appliance appliance, float deltaPercentage)
+        {
+            LogInfo("Action: DecrementPercentage");
+
+            var request = new Control.DecrementPercentageRequest
+            {
+                AccessToken = accessToken,
+                Appliance = appliance,
+                DeltaPercentage = new ControlParameter(deltaPercentage),
+            };
+
+            var response = ProgressMessage(request) as Control.DecrementPercentageConfirmation;
+        }
+
+        //Health Check Messages
+        public bool HealthCheck(long initiationTimestamp, out string description)
+        {
+            LogInfo("Action: HealthCheck");
+
+            var request = new System.HealthCheckRequest
+            {
+                InitiationTimestamp = initiationTimestamp.ToString(),
+            };
+
+            var response = ProgressMessage(request) as System.HealthCheckResponse;
+            description = response.Description;
+            return response.IsHealthy;
+        }
+    }
+}
